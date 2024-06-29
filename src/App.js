3fdfc745cdae7e1638046f52133031e4e8ff6f76
kashisharya <aryak0823@gmail.com>
2024-06-29 15:19:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import WeatherApp from './components/WeatherApp/WeatherApp';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div className="App">
+     <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
